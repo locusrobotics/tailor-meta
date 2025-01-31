b71199ca5286aa0926559eb62f06dac6a14a8cd7
Kalpesh <klad@locusrobotics.com>
2025-01-31 14:02:33 -0500

    Change src to be devel instead of master

--- vars/checkoutPR.groovy
+++ vars/checkoutPR.groovy
@@ -3,7 +3,7 @@ def call(String prNbr, String repo) {
         branches: [[name: "FETCH_HEAD"]],
         doGenerateSubmoduleConfigurations: false,
         extensions: [[$class: 'LocalBranch'], [$class: 'RelativeTargetDirectory', relativeTargetDir: "${repo}"]],
-        userRemoteConfigs: [[refspec: "+refs/pull/${prNbr}/head:refs/remotes/origin/PR-${prNbr} +refs/heads/master:refs/remotes/origin/master",
+        userRemoteConfigs: [[refspec: "+refs/pull/${prNbr}/head:refs/remotes/origin/PR-${prNbr} +refs/heads/devel:refs/remotes/origin/devel",
                             url: "https://${this.env.GITHUB_TOKEN}@github.com/locusrobotics/${repo}"]]
     ])
 }
