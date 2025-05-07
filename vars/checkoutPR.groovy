86cd626cc2f7f17c80abcb0de98e3c3558422fb4
Kalpesh Lad <klad@locusrobotics.com>
2025-05-07 14:33:12 -0400

    Clean-up RemoteConfig

--- vars/checkoutPR.groovy
+++ vars/checkoutPR.groovy
@@ -1,9 +1,9 @@
 def call(String prNbr, String repo) {
     checkout([$class: 'GitSCM',
-        branches: [[name: branch]],
+        branches: [[name: "FETCH_HEAD"]],
         doGenerateSubmoduleConfigurations: false,
         extensions: [[$class: 'LocalBranch'], [$class: 'RelativeTargetDirectory', relativeTargetDir: "${repo}"]],
-        userRemoteConfigs: [[refspec: "+refs/pull/*:refs/remotes/origin/PR-* +refs/heads/devel:refs/remotes/origin/devel",
+        userRemoteConfigs: [[refspec: "+refs/pull/${pr_num}/head:refs/remotes/origin/PR-${pr_num} +refs/heads/devel:refs/remotes/origin/devel",
                             url: "https://${this.env.GITHUB_TOKEN}@github.com/locusrobotics/${repo}"]]
     ])
 }
