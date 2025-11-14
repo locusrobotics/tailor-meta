c715f5da8283f7fd19c04b132a95f69467f3cdae
James Prestwood <jprestwood@locusrobotics.com>
2025-11-14 11:25:11 -0800

    Add separate groovy file for per package builds

---
+++ vars/perPackageBuild.groovy
@@ -0,0 +1,40 @@
+def call(Map args) {
+  String tailor_distro = args['versions'].get('tailor_distro')
+  String tailor_image = args['versions'].get('tailor_image')
+  String tailor_meta = args['versions'].get('tailor_meta')
+
+  pipeline {
+    agent any
+
+    environment {
+        PYTHON = config.get('python', '/usr/bin/python3')
+    }
+
+    stages {
+      stage('Clone Repository') {
+        steps {
+          git branch: 'build-per-package', url: 'https://github.com/locusrobotics/tailor-distro.git')
+        }
+      }
+
+      stage('Set Up Virtual Environment') {
+          steps {
+              sh """
+                  ${PYTHON} -m venv venv
+                  source venv/bin/activate
+                  pip install --upgrade pip
+              """
+          }
+      }
+
+      stage('Build Package') {
+          steps {
+              sh """
+                  source venv/bin/activate
+                  pip install .
+              """
+          }
+      }
+    }
+  }
+}
