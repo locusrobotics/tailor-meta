6ae1a19cbb6e91164b9587198ddc5d989902b7fd
James Prestwood <jprestwood@locusrobotics.com>
2025-11-18 11:21:26 -0800

    Add __init__ bits

--- tailor_meta/__init__.py
+++ tailor_meta/__init__.py
@@ -1,9 +1,16 @@
 import argparse
 import pathlib
+import json
+import re
+import subprocess
+import sys
 try:
     import yaml
 except ModuleNotFoundError:
     pass
+
+from collections import namedtuple
+from typing import Iterable, List
 
 __version__ = '0.0.0'
 
@@ -16,3 +23,105 @@ class YamlLoadAction(argparse.Action):
 
     def __call__(self, parser, namespace, value, option_string=None):
         setattr(namespace, self.dest, yaml.safe_load(pathlib.Path(value).open()))
+
+SCHEME_S3 = "s3://"
+
+
+def get_bucket_name(apt_repo):
+    assert(apt_repo.startswith(SCHEME_S3))
+    return apt_repo[len(SCHEME_S3):]
+
+
+def run_command(cmd, *args, **kwargs):
+    print(' '.join(cmd), file=sys.stderr)
+    if kwargs.pop('capture_output', False):
+        kwargs['stdout'] = subprocess.PIPE
+        kwargs['stderr'] = subprocess.PIPE
+    return subprocess.run(cmd, check=True, *args, **kwargs)
+
+
+def gpg_import_keys(keys: Iterable[pathlib.Path]) -> None:
+    """Import gpg key from path."""
+    for key in keys:
+        run_command(['gpg1', '--import', str(key)])
+
+
+def get_gpg_key_id() -> str:
+    """Get gpg's key id."""
+    gpg_key = run_command(['gpg1', '--fingerprint'], capture_output=True)
+    gpg_key = gpg_key.stdout.decode('utf-8').split('\n')
+    gpg_key = ''.join([line for line in gpg_key if 'Key fingerprint' in line])
+    return ''.join(gpg_key.split(' ')[-2:])
+
+
+def aptly_configure(apt_repo, release_label):
+    bucket_name = get_bucket_name(apt_repo)
+    aptly_endpoint = f"s3:{bucket_name}:{release_label}/ubuntu/"
+
+    aptly_config = {
+        "gpgProvider": "internal",
+        "dependencyFollowSuggests": True,
+        "dependencyFollowRecommends": True,
+        "dependencyFollowAllVariants": True,
+        "S3PublishEndpoints": {
+            bucket_name: {
+                "region": "us-east-1",
+                "bucket": bucket_name,
+                "acl": "private",
+                "debug": False
+            }
+        }
+    }
+
+    with open(pathlib.Path.home() / ".aptly.conf", mode='w') as aptly_config_file:
+        json.dump(aptly_config, aptly_config_file)
+
+    return aptly_endpoint
+
+
+def deb_s3_common_args(apt_repo: str, os_name: str, os_version: str, release_label: str) -> List[str]:
+    bucket_name = get_bucket_name(apt_repo)
+    return [
+        f'--bucket={bucket_name}',
+        f'--origin={apt_repo}',
+        f'--prefix={release_label}/{os_name}',
+        f'--codename={os_version}',
+        f'--suite={os_version}',
+    ]
+
+
+whitespace_regex = re.compile(r'\s+')
+PackageEntry = namedtuple("PackageEntry", "name version arch")
+
+
+def deb_s3_list_packages(common_args: Iterable[str]) -> List[PackageEntry]:
+    command = [
+        'deb-s3', 'list',
+    ]
+    command.extend(common_args)
+    stdout = run_command(command, stdout=subprocess.PIPE).stdout.decode()
+    package_lines = stdout.strip().splitlines()
+    return [PackageEntry(*whitespace_regex.split(line)) for line in package_lines]
+
+
+def deb_s3_upload_packages(package_files: Iterable[pathlib.Path], visibility: str, common_args: Iterable[str]):
+    gpg_key = get_gpg_key_id()
+    command = [
+        'deb-s3', 'upload',
+        *map(str, package_files),
+        f'--visibility={visibility}', f'--sign={gpg_key}', '--gpg-provider=gpg1', '--preserve-versions'
+    ]
+    command.extend(common_args)
+    run_command(command)
+
+
+def deb_s3_delete_packages(packages: Iterable[PackageEntry], visibility: str, common_args: Iterable[str]):
+    gpg_key = get_gpg_key_id()
+    for package in packages:
+        command = [
+            'deb-s3', 'delete', package.name,
+            f'--versions={package.version}', f'--arch={package.arch}', '--do-package-remove',
+            f'--visibility={visibility}', f'--sign={gpg_key}', '--gpg-provider=gpg1'
+        ]
+        command.extend(common_args)
+        run_command(command)
