161b8b68278e24967fef532f98926b028fea52e3
James Prestwood <jprestwood@locusrobotics.com>
2025-11-20 06:03:26 -0800

    use parentImage with branch name

--- vars/perPackageBuild.groovy
+++ vars/perPackageBuild.groovy
@@ -16,8 +16,7 @@ def src_dir = workspace_dir + '/src'
 def debian_dir = workspace_dir + '/debian'
 
 def srcStash = { release -> release + '-src' }
-//def parentImage = { release, docker_registry -> docker_registry - "https://" + ':tailor-distro-' + release + '-parent-' + env.BRANCH_NAME }
-def parentImage = { release, docker_registry -> docker_registry - "https://" + ':tailor-distro-' + release + '-parent-' + 'per-package-build' }
+def parentImage = { release, docker_registry -> docker_registry - "https://" + ':tailor-distro-' + release + '-parent-' + env.BRANCH_NAME }
 def bundleImage = { release, os_version, docker_registry -> docker_registry - "https://" + ':tailor-distro-' + release + '-bundle-' + os_version + '-' + env.BRANCH_NAME }
 def debianStash = { recipe -> recipe + "-debian"}
 def packageStash = { recipe -> recipe + "-packages"}
