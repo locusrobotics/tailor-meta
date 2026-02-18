d428ae4c8c2c989d3e622d16cbd3e792f65a2d02
Bianca Bendris <bbendris@locusrobotics.com>
2026-02-18 11:39:46 +0100

    Set the property directly, it doesn't exist yet

--- tailor_meta/update_repo_settings.py
+++ tailor_meta/update_repo_settings.py
@@ -101,22 +101,11 @@ def update_repo_settings(rosdistro_index: pathlib.Path, recipes: Mapping[str, An
                     )
             # Set custom property default-not-protected to true
             if deploy:
-                existing_props = {
-                    p.property_name: p.value
-                    for p in gh_repo.get_custom_properties()
-                }
-
-                if "default-not-protected" in existing_props:
-                    gh_with_retry(
-                        github_client,
-                        gh_repo.update_custom_properties,
-                        {"default-not-protected": "true"},
-                    )
-                else:
-                    click.echo(
-                        f"Custom property 'default-not-protected' not defined for {gh_repo.full_name}",
-                        err=True,
-                    )
+                gh_with_retry(
+                    github_client,
+                    gh_repo.update_custom_properties,
+                    {"default-not-protected": "true"},
+                )
 
 
 def main():
