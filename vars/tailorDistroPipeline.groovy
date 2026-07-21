5a9873ce18a76da8d5e0d6c43ab5b76facff3f5b
James Prestwood <jprestwood@locusrobotics.com>
2026-07-21 09:11:22 -0700

    Add forgotten comma

--- vars/tailorDistroPipeline.groovy
+++ vars/tailorDistroPipeline.groovy
@@ -109,7 +109,7 @@ def call(Map args) {
       booleanParam(name: 'invalidate_docker_cache', value: params.invalidate_docker_cache),
       booleanParam(name: 'per_package_build', value: per_package_build),
       string(name: 'apt_refresh_key', value: weekNum),
-      booleanParam(name: 'invalidate_colcon_cache', value: params.invalidate_colcon_cache)
+      booleanParam(name: 'invalidate_colcon_cache', value: params.invalidate_colcon_cache),
       booleanParam(name: 'overwrite_release_label', value: params.overwrite_release_label)
     ]
   }
